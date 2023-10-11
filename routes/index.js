4918d1d921e874352b6d3c3e402477658ad4242f
mlehr3 <mlehr3@outlook.com>
2023-10-11 15:38:22 -0400

    Create the Root URL Route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var path = require('path');
+
+//create a '/' GET route that'll return the index.html file stored in the public/html folder
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.sendFile(path.join(__dirname, '..','/public/html/index.html'));
+}); 
 
 module.exports = router;
